f0db0fd8e4c8a7204dd8c9aebe9118f1e75bf98d
SquireoH <155275477+SquireoH@users.noreply.github.com>
2024-02-19 23:33:19 -0500

    Update driftscore.lua

--- driftscore.lua
+++ driftscore.lua
@@ -75,7 +75,7 @@ function script.update(dt)
         addMessage("Car is outside", -1)
         wheelsWarningTimeout = 60
     end
-    if math.abs(player.slipAngle) > 10 then
+    if math.abs(player.slipRatio) > 0.1 then
         totalScore = totalScore + 1
         if player.speedKmh > 50 then
             comboProgress = comboProgress + 0.1
